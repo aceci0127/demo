e6f28952321b406f2e78230383b115c30410045a
aceci0127 <alfredo.ceci@studbocconi.it>
2025-01-23 14:27:22 +0100

    Commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ index_body = "papers-body-packaging"
 index_abstract = "papers-abstracts"
 
 # ----------- Streamlit UI & main logic -----------
-st.image("images/Logo.png", width=78)
+st.image("images/Logo.png", width=100)
 st.title("A T H E N A | Packaging DEMO")
 st.info("Welcome to *ATHENA*, the AI-powered search engine for scientific papers on Packaging. The dataset is composed of only 100 scientific papers.  - ENGLISH ONLY🇬🇧")
 
