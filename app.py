7f78450be203d6a749d6365cbb285c515abec602
aceci0127 <alfredo.ceci@studbocconi.it>
2025-02-18 20:19:08 +0100

    Commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Set up page configuration
 st.set_page_config(
-    page_title="Your Startup - Athena Demo",
+    page_title="ATHENA Demo",
     page_icon=":rocket:",
     layout="wide",
 )
