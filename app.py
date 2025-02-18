2acc18db03255af3fd311e75d0650f9bbfcc7d41
aceci0127 <alfredo.ceci@studbocconi.it>
2025-02-18 20:27:14 +0100

    Commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-Home = st.Page("app.py", title="Homepage", icon=":material/home:")
+Home = st.Page("pages/Homepage.py", title="Homepage", icon=":material/home:")
 Pack = st.Page("pages/Packaging.py", title="Packaging Demo")
 Med = st.Page("pages/Medical.py", title="Medical Demo")
 
