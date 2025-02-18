264f9d88dc8c3938bfdcbe01502f765c21d74b9c
aceci0127 <alfredo.ceci@studbocconi.it>
2025-02-18 20:38:53 +0100

    Commit

--- pages/Packaging.py
+++ pages/Packaging.py
@@ -9,7 +9,7 @@ index_entity = "papers-entity-embeddings"
 
 # ----------- Streamlit UI & main logic -----------
 st.image("images/Logo.png", width=100)
-st.title("A T H E N A | Packaging DEMO")
+st.title("A T H E N A | Packaging")
 st.info("The dataset is composed of only 100 scientific papers.  - ENGLISH ONLY🇬🇧")
 
 # Initialize the conversation if not present
