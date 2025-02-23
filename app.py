0a747cd74d1ee9a9288a5e04efc8e190923a34d5
aceci0127 <alfredo.ceci@studbocconi.it>
2025-02-23 17:10:44 +0100

    Commit

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ Pack = st.Page("pages/Packaging.py", title="Packaging")
 Cop = st.Page("pages/Copper2.py", title="Copper")
 Ther = st.Page("pages/ThermalB.py", title="Thermal Barrier")
 
-
 pg = st.navigation([Home, Pack, Cop, Ther])
-
 
 pg.run()
