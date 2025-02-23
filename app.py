1b3600bd08502fa5076e575a5375c30322e1d4dc
aceci0127 <alfredo.ceci@studbocconi.it>
2025-02-23 16:59:17 +0100

    Commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 Home = st.Page("pages/Homepage.py", title="Homepage", icon=":material/home:")
 Pack = st.Page("pages/Packaging.py", title="Packaging")
-Cop = st.Page("pages/Copper.py", title="Copper")
+Cop = st.Page("pages/Copper2.py", title="Copper")
 Ther = st.Page("pages/ThermalB.py", title="Thermal Barrier")
 
 
