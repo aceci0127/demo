2955c3c7e208ea45fb613524720d3f8ee352393f
aceci0127 <alfredo.ceci@studbocconi.it>
2025-02-23 13:09:35 +0100

    Commit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ Home = st.Page("pages/Homepage.py", title="Homepage", icon=":material/home:")
 Pack = st.Page("pages/Packaging.py", title="Packaging")
 Med = st.Page("pages/Medical.py", title="Medical")
 Ther = st.Page("pages/ThermalB.py", title="Thermal Barrier")
+st.markdown('<link href="https://fonts.cdnfonts.com/css/glacial-indifference" rel="stylesheet">', unsafe_allow_html=True)
 
 pg = st.navigation([Home, Pack, Med, Ther])
 
